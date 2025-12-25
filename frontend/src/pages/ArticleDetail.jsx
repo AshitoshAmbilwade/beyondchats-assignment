29b20062922e07036587b67df5a9c25bf88c4144
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-12-25 17:24:04 +0530

    Article Detailed done

--- frontend/src/pages/ArticleDetail.jsx
+++ frontend/src/pages/ArticleDetail.jsx
@@ -1,28 +1,34 @@
-<main className="container">
-  <h1>{article.title}</h1>
+const ArticleDetail = () => {
+  return (
+    <main className="container">
+      <h1>{article.title}</h1>
 
-  <span className={`badge ${article.type}`}>
-    {article.type}
-  </span>
+      <span className={`badge ${article.type}`}>
+        {article.type}
+      </span>
 
-  <div className="content">
-    {article.content.split("\n").map((p, i) => (
-      <p key={i}>{p}</p>
-    ))}
-  </div>
-
-  {article.references && (
-    <>
-      <h3 style={{ marginTop: "40px" }}>References</h3>
-      <ul>
-        {article.references.map((ref, i) => (
-          <li key={i}>
-            <a href={ref} target="_blank" rel="noreferrer">
-              {ref}
-            </a>
-          </li>
+      <div className="content">
+        {article.content.split("\n").map((p, i) => (
+          <p key={i}>{p}</p>
         ))}
-      </ul>
-    </>
-  )}
-</main>
+      </div>
+
+      {article.references && (
+        <>
+          <h3 style={{ marginTop: "40px" }}>References</h3>
+          <ul>
+            {article.references.map((ref, i) => (
+              <li key={i}>
+                <a href={ref} target="_blank" rel="noreferrer">
+                  {ref}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default ArticleDetail;
